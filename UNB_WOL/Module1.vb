35e15fc4de4ffac053931939b645e60ac46a4170
Uwe-Niemann <uniemann@gmx.de>
2021-09-12 18:07:13 +0200

    UNB: Wake on lan

---
+++ UNB_WOL/Module1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+
+Module Module1
+    Sub Main()
+        If System.Environment.GetCommandLineArgs().Length > 1 Then
+            Dim args() As String = {}
+            Try
+                args = Split(System.Environment.CommandLine, "/")
+                If args.Length > 1 Then
+                    WakeUp(args(1))
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.OkOnly + MsgBoxStyle.Information, "Wake on lan")
+            Finally
+                Array.Resize(Of String)(args, 0)
+            End Try
+        End If
+    End Sub
+
+    Private Const MAC_ADDR_BYTES As Integer = 6
+    Private Const PORT_BROADCAST = 2304
+    ' <remarks>
+    ' Constructs and returns a magic packet for the given 
+    ' MAC address.
+    ' A Magic Packet is 6 bytes of FF followed by the MAC 
+    ' address 16 times.
+    ' </remarks>
+    Private Function GetMagicPacket(ByVal macAddress As String) As Byte()
+        Dim Packet As Byte() = New Byte(5 + 16 * MAC_ADDR_BYTES) {} '101 => 102 Elements
+        Dim strNumbers As String() = macAddress.Split(New Char() {":", ",", ";", "-"})
+        Dim macBytes As Byte() = New Byte(5) {}
+        If strNumbers.Length <> 6 Then
+            Throw New Exception("MAC Address incorrect!!!")
+        End If
+        'Convert Numbers to Bytes and set the first 6 FF Values
+        For i As Integer = 0 To 5
+            Packet(i) = &HFF
+            Dim strNumber As String = strNumbers(i)
+            'Strip possible leading 0x statments
+            If strNumber.StartsWith("0x") Then
+                strNumber = strNumber.Substring(2, 2)
+            End If
+            macBytes(i) = CByte(Int32.Parse(strNumber, System.Globalization.NumberStyles.HexNumber))
+        Next i
+        'Write the MAC address 16 times after the 6 FF values
+        For j As Integer = 0 To 15
+            For i As Integer = 0 To 5
+                Packet(6 + j * 6 + i) = macBytes(i)
+            Next i
+        Next j
+        Return Packet
+    End Function
+
+    '<remarks>
+    'Sends the magic packet for a specific MAC address
+    '</remarks>
+    Private Sub WakeUp(ByVal macAddress As String)
+        Dim s As Socket = New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+        s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1)
+        Dim Message As Byte() = GetMagicPacket(macAddress)
+        Dim IPEP As New IPEndPoint(IPAddress.Broadcast, PORT_BROADCAST)
+        '
+        s.SendTo(Message, IPEP)
+    End Sub
+End Module
---
+++ UNB_WOL/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("UNB_WOL")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UNB_WOL")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("0856c160-a232-48bd-9913-02f13a8d0437")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
